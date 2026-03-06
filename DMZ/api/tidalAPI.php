de1a22d17e9061c9944600f1626f3ea2d69e3cb1
Kim Corpuz <kc648@njit.edu>
2026-03-06 18:26:43 +0000

    add DMZ and api folders. I also added dmzListener, and tidalAPI for the logic

---
+++ DMZ/api/tidalAPI.php
@@ -0,0 +1,54 @@
+<?php
+
+function tokenForUser() {
+
+    $clientID = "TNi2hY6txPCXnDAA";
+    $secretID = "xYgoYGl4KVYvvPpGJ8Fb4Ljucjvv8KzTfmyRBLrj52A=";
+
+    $authCredentials = base64_encode($clientID . ":" . $secretID);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://auth.tidal.com/v1/oauth2/token");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Basic $authCredentials", "Content-Type: application/x-www-form-urlencoded"));
+ 
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $decodedResponse = json_decode($response, true);
+    print_r($decodedResponse);
+
+    return $decodedResponse['access_token'];
+
+}
+function searchArtist($artistName, $countryCode = "US") {
+
+    $tokenTidal = tokenForUser();
+
+   // $url = "https://openapi.tidal.com/v2/searchResults/" . urlencode($artistName) . "?countryCode" . $countryCode . "&explicitFilter=INCLUDES&include=albums";
+
+    $url = "https://openapi.tidal.com/v2/searchResults?query=" . urlencode($artistName) . "&countryCode=US&include=artists";
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $tokenTidal", "Accept: application/vnd.api.v1+json", "Content-Type: application/vnd.api.v1+json"));
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        error_log('cURL Error: ' . curl_error($ch));
+        exit('Sorry! An error occurred.');
+
+}
+    curl_close($ch);
+
+    $decodedResponse = json_decode($response, true);
+    print_r($decodedResponse);
+
+    return $decodedResponse;
+}
+
---
+++ DMZ/testTitalAPI.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ('api/tidalAPI.php');
+
+$result = searchArtist("jayz");
+
+print_r($result);
+
+?>
