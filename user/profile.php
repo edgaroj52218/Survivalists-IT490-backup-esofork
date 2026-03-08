b2721901f439e2477eb72f20fc2ac4a976eabc83
Kim Corpuz <kc648@njit.edu>
2026-03-08 21:19:36 +0000

    user directory created w/ initial profile page

---
+++ user/profile.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profile | SocialTune</title>
+    </head>
+
+    <style type="text/css">
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+
+        #teal_bar {
+            height: 50px;
+            background-color: #FDF5DF;
+            color: #5EBEC4;
+        }
+
+        #search_bar {
+            width: 400px;
+            height: 20px;
+            border-radius: 5px;
+            border: none;
+            padding: 4px;
+            font-size: 14px;
+            background-image: url(../images/magnifying_glass.jpeg);
+        }
+    </style>
+
+    <body style="font-family: 'Playfair Display', serif;">
+
+        <div id="teal_bar">
+            <div style="width: 800px; margin-auto: auto; font-size: 30px">
+                SocialTune &nbsp &nbsp <input type="text" id="search_bar" placeholder="Search for tracks, artists, or albums>
+            </div>
+        </div>
+
+    </body>
+
+</html>
