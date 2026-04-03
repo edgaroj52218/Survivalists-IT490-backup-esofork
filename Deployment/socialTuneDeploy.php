3f99ddf24911e0e5ce8c351752a8e58a14d3e020
Katerine Gomez <kathegomez0756@gmail.com>
2026-04-03 13:56:01 -0400

    i updated the socialTuneDeploy file so i can set the package status and i can trigger the deployment throught rabbitmq

--- Deployment/socialTuneDeploy.php
+++ Deployment/socialTuneDeploy.php
@@ -15,6 +15,23 @@ $mongoClient = new MongoDB\Client($uri);
 $database = $mongoClient->SocialTuneDeployment;
 $collection = $database->packages;
 
+// info for the package
+$bundleName = "socialtune";
+$version = "1.0";
+$status = "new";
 
+// in this part im gonna insert the info to the package
+$collection->updateOne(array("bundleName" => $bundleName, "version" => $version), array('$set' => array("status" => $st>
+
+
+
+$client = new rabbitMQClient("testRabbitMQ.ini", "testDeployment");
+
+$request = array("type" => "install", "bundle" => $bundleName, "version" => $version, "environment" => "QA");
+
+$response = $client->send_request($request);
+
+// printing the output
+print_r(array("message" => "Deployment triggered", "bundle" => $bundleName, "version" => $version, "status" => $status,>
 
 ?>
