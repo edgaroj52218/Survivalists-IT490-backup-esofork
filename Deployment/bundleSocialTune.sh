65b63ac353b3ae1ecbe3e0dbf3bb5e0a054c923e
Katerine Gomez <kathegomez0756@gmail.com>
2026-04-06 09:03:55 -0400

    in this part now i create the .json with all the copy files, the commands, and what the installer needs.

--- Deployment/bundleSocialTune.sh
+++ Deployment/bundleSocialTune.sh
@@ -18,4 +18,28 @@ deployUser="katerinegomez"
 deployHost="100.97.21.49"
 deployPath="/home/katerinegomez/bundles"
 
+# this will remove the old folder only if it exists and then it will make a new one
+echo "=> we're creating the build directory..."
+rm -rf $buildDir
+mkdir -p $buildDir/files
 
+# this will copy everyhing that we have to the new folder
+echo "=> we're copying the project files..."
+cp -r $sourceDir/* $buildDir/files/
+
+echo "=>we're creating the deploy config..."
+
+# this part will create the deploy.json so the instller knows to do
+# i used this reference for cat eOF: https://stackoverflow.com/questions/2500436/how-does-cat-eof-work-in-bash
+cat <<EOF > $buildDir/deploy.json
+{
+  "copyFiles": [
+    {
+      "source": "files/",
+      "destination": "/var/www/html/"
+    }
+  ],
+  "commands": ["echo Deployment completed"],
+  "services": ["apache2"]
+}
+EOF
