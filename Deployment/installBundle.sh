624f0bfe64ea4af37b06240c6e9bfcdac4306bc1
Katerine Gomez <kathegomez0756@gmail.com>
2026-04-06 09:53:05 -0400

    on this part the script now creates a temporary folder, it downloads the bundle and then extracts it.

--- Deployment/installBundle.sh
+++ Deployment/installBundle.sh
@@ -13,3 +13,32 @@ bundleFile=$1
 deployUser="katerinegomez"
 deployHost="100.97.21.49"
 deployPath="/home/katerinegomez/bundles"
+
+# this will create the temporary file so it doesnt  conflict wiht the ones that are old.
+temporaryDir="/tmp/deploy_$(date +%s)"
+
+echo "=> we're creating the temporary directory..."
+mkdir -p $temporaryDir
+
+# this will download the bundle tp the temporary folder
+echo "=> we're downloading the bundle..."
+scp $deployUser@$deployHost:$deployPath/$bundleFile $temporaryDir/
+
+# here if the last commnad wasnt successful, im printing an error and it will quit
+# i used this reference to understand the bash variables: https://tecadmin.net/bash-special-variables/
+if [ $? -ne 0 ]; then
+  echo "=> the download failed"
+  exit 1
+fi
+
+cd $temporaryDir
+
+echo "=> we're extracting the bundle..."
+tar -xzf $bundleFile
+
+if [ $? -ne 0 ]; then
+  echo "=> sorry, the extraction failed"
+  exit 1
+fi
+
+echo "=> we're reading the deploy.json..."
