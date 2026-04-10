f7a781573c0b89bb76dcd09b7522f15314d303a5
Katerine Gomez <kathegomez0756@gmail.com>
2026-04-10 10:29:54 -0400

    i changed the install file to make it more simple since it was failing, and i focused on basic extraction

--- Deployment/installBundle.sh
+++ Deployment/installBundle.sh
@@ -3,67 +3,57 @@
 # i used this website for the bash commands: https://www.gnu.org/software/bash/manual/bash.html
 # i used this website for the bash test operators: https://linuxhandbook.com/bash-test-operators/
 
-# this checks the bundle name
-if [ -z "$1" ]; then
-  echo "provide bundle name, example: ./installBundle.sh <bundle-name>"
+# when somethiing fails, set -e stops the script stop 
+set -e
+
+BUNDLE=$1
+BUNDLE_PATH="/home/katerinegomez/bundles"
+DEST="/var/www/html"
+
+echo "=> starting the  deployment..."
+
+# here cheking if the bundle exists
+if [ ! -f "$BUNDLE_PATH/$BUNDLE" ]; then
+  echo "=> error: The bundle was not found!"
   exit 1
 fi
-
-bundleFile=$1
-deployUser="katerinegomez"
-deployHost="100.97.21.49"
-deployPath="/home/katerinegomez/bundles"
 
 # this will create the temporary file so it doesnt  conflict wiht the ones that are old.
-temporaryDir="/tmp/deploy_$(date +%s)"
+TEMP="/tmp/deploy_$(date +%s)"
+EXTRACT_DIR="$TMP/extracted"
 
 echo "=> we're creating the temporary directory..."
-mkdir -p $temporaryDir
+mkdir -p $EXTRACT_DIR
 
-# this will download the bundle tp the temporary folder
-echo "=> we're downloading the bundle..."
-scp $deployUser@$deployHost:$deployPath/$bundleFile $temporaryDir/
+echo "=> we're copying the bundle..."
+cp $BUNDLE_PATH/$BUNDLE $TMP/
+cd $TMP
 
-# here if the last commnad wasnt successful, im printing an error and it will quit
-# i used this reference to understand the bash variables: https://tecadmin.net/bash-special-variables/
-if [ $? -ne 0 ]; then
-  echo "=> the download failed"
-  exit 1
+echo "=> we're extracting bundle..."
+# TYPE=$(file --mime-type -b $BUNDLE)
+
+# reference i used to extract the tar files: https://linuxize.com/post/how-to-extract-unzip-tar-gz-file/
+tar -xzf $BUNDLE -C $EXTRACT_DIR
+
+echo "=> we're cleaning the old files..."
+sudo rm -rf $DEST/*
+
+echo "=> we're copying the new files..."
+
+# using the if statemnt because there a cases where the bundle we're sending sometiems has files/ folder and other times not 
+if [ -d "$EXTRACT_DIR/files" ]; then
+  sudo cp -r $EXTRACT_DIR/files/* $DEST/
+else
+  sudo cp -r $EXTRACT_DIR/* $DEST/
 fi
 
-cd $temporaryDir
+echo "=> we're setting the permissions..."
+# i used this reference so i can implement  the chmod and chown in the right way: https://linuxize.com/post/linux-chown-command/
+sudo chown -R www-data:www-data $DEST
+sudo chmod -R 755 $DEST
 
-echo "=> we're extracting the bundle..."
-tar -xzf $bundleFile
-
-if [ $? -ne 0 ]; then
-  echo "=> sorry, the extraction failed"
-  exit 1
-fi
-
-echo "=> we're reading the deploy.json..."
-
-# i used this reference to understand jq better: https://jqlang.org/manual/
-# this part will copy all the files -- note: -c means compact aand helps in this case so it doesnt break the loop -- note: -r will remove the quotes so the cp doesnt get confused
-jq -c '.copyFiles[]' deploy.json | while read item; do
-  SRC=$(echo $item | jq -r '.source')
-  DEST=$(echo $item | jq -r '.destination')
-
-  echo "=> we're copying $SRC to $DEST"
-  sudo cp -r $SRC/* $DEST
-done
-
-# this will run the commands
-jq -r '.commands[]' deploy.json | while read cmd; do
-  echo "=> we're running: $cmd"
-  # eval helps to run the command from the json
-  eval $cmd
-done
-
-# this will restart the services 
-jq -r '.services[]' deploy.json | while read service; do
-  echo "=> we're restarting: $service"
-  sudo systemctl restart $service
-done
+echo "=? we're restarting Apache..."
+# the changes need to happn so i restart apache
+sudo systemctl restart apache2
 
 echo "=> congrats! The deployment was completed successfully!"
