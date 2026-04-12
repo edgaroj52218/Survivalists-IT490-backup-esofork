7eda2efe46c536ece570fd3dcab24db2bca0e333
Katerine Gomez <kathegomez0756@gmail.com>
2026-04-12 12:19:16 -0400

    i fixed the increment because i was getting null results on mongodb, when i was sending a package so set the default number to 0 so it starts on 1. I also add upsert so it adds on mongo.

--- Deployment/incrementBundle.php
+++ Deployment/incrementBundle.php
@@ -1,9 +1,6 @@
 #!/usr/bin/php
 <?php
 
-require_once('../path.inc');
-require_once('../get_host_info.inc');
-require_once('../rabbitMQLib.inc');
 require_once('vendor/autoload.php');
 
 $uri = 'mongodb://100.97.21.49:27017/';
@@ -14,10 +11,17 @@ $collection = $mongoClient->SocialTuneDeployment->versionCounter;
 // here were getting the last version
 $lastVersion = $collection->findOne(array("name" => "socialtune"));
 
-// incrementing by one 
-$newVersion = intval($lastVersion["version"]) + 1;
+// if it is null i add 0 so the increment starts at 1
 
-$collection->updateOne(array("name" => "socialtune"), array('$set' => array("version" => $newVersion)));
+if ($lastVersion === null) {
+    $numberVersion = 0;
+} else {
+    $numberVersion = intval($lastVersion["version"]);
+}
+
+$newVersion = $numberVersion + 1;
+
+$collection->updateOne(array("name" => "socialtune"), array('$set' => array("version" => $newVersion)), array('upsert'=>true));
 echo $newVersion;
 
 ?>
