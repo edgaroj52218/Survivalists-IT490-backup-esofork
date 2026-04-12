81a2694c2ab6cb1af8a28ec2b0cd604c0349507a
Katerine Gomez <kathegomez0756@gmail.com>
2026-04-12 10:31:29 -0400

    i deleted on of the argv and i inserted it into the collection in our db. I also fixed some systax errors

--- Deployment/socialTuneDeploy.php
+++ Deployment/socialTuneDeploy.php
@@ -13,30 +13,16 @@ $bundle = $argv[1];
 $version = $argv[2];
 $env = strtoupper($argv[3]);
 
-
-// trying to do failed and passed
-$status = $argv[4] ?? "new";
-
 // Connection to the deployment mongo
 $uri = 'mongodb://100.97.21.49:27017/';
 $mongoClient = new MongoDB\Client($uri);
-
 $database = $mongoClient->SocialTuneDeployment;
 $collection = $database->packages;
 
-// this allows me to insert the new arriving version to db 
-if ($status == "new") {
-   $collection->insertOne(["bundle"=>$bundle, "version"=>$version, "status"=>"new", "env"=>$env, "timestamp"=>time()]);
-} else {
-   $collection->insertOne(["bundle"=>$bundle, "version"=>$version, "status"=>$status, "env"=>$env, "timestamp"=>time()]);
-}
+$collection->insertOne(["bundle"=>$bundle, "version"=>$version, "status"=>"new", "env"=>$env, "timestamp"=>time()]);
 
-client = new rabbitMQClient("testRabbitMQ.ini","deploymentServer");
-
+$client = new rabbitMQClient("testRabbitMQ.ini","deploymentServer");
 $request = ["type"=>"install","bundle"=>$bundle,"version"=>$version,"environment"=>$env];
-
 $response = $client->send_request($request);
-
-echo "Deployment triggered\n";
 
 ?>
